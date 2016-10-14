af1a6507fc6151769b8b25e8d41949f4eb91ee96
Jeremy Bowles <jeremybo@wiggum.local>
2016-10-14 09:19:57 +0100

    var typo

--- iam.tf
+++ iam.tf
@@ -13,7 +13,7 @@ data "template_file" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment" {
-  count      = "${length(iam_policy_count)}"
+  count      = "${length(var.iam_policy_count)}"
   role       = "${aws_iam_role.iam_role.name}"
   policy_arn = "${var.iam_policy_arns[count.index]}"
 }
